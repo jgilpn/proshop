e8b4977c7ba78181dbe79d03c832a3387a3a0b87
John <jgilpn@gmail.com>
2020-12-31 12:55:27 -0800

    Basic frontend setup that displays products requested from backend, setup with basic endpoints

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
+import Product from '../components/Product'
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/v1/products')
+
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, [])
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default HomeScreen
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import axios from 'axios'
+import Rating from '../components/Rating'
+
+const ProductScreen = ({ match }) => {
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/v1/products/${match.params.id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+  }, [])
+
+  return (
+    <>
+      <Link className='my-3' to='/'>
+        <Button variant='light'>Go Back</Button>
+      </Link>
+      <Row>
+        <Col md={12} lg={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={12} lg={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={12} lg={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  variant='dark'
+                  block
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default ProductScreen
