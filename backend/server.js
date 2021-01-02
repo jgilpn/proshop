16ad3e1c0346bd0052590acffe6e175e359d00c8
John <jgilpn@gmail.com>
2021-01-02 02:22:01 -0800

    added POST route for addOrderItems

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 // Uses the .env file
 dotenv.config()
@@ -25,6 +26,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/products', productRoutes)
 app.use('/api/v1/users', userRoutes)
+app.use('/api/v1/orders', orderRoutes)
 
 // Error Middleware
 app.use(notFound)
