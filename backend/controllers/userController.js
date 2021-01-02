8c72996be14000cbee6d4ad2bdee380c4dc8a1ca
John <jgilpn@gmail.com>
2021-01-01 19:45:08 -0800

    added auto password hashing to userSchema, added registerUser enpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,9 @@ const authUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email })
 
+  // Check if user w/ email and password was found
   if (user && (await user.matchPassword(password))) {
+    // Send back user data and token
     res.json({
       _id: user._id,
       name: user.name,
@@ -19,8 +21,47 @@ const authUser = asyncHandler(async (req, res) => {
       token: generateToken(user._id),
     })
   } else {
+    // Otherwise 401 Error
     res.status(401)
     throw new Error('Invalid email or password')
+  }
+})
+
+// @desc    Register a new user
+// @route   POST /api/v1/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  // If user already exists send 400 Error
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Create new user in DB
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  // Check if user was created
+  if (user) {
+    // Send back user data and token
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    // Otherwise send 400 Error
+    res.status(400)
+    throw new Error('Invalid user data')
   }
 })
 
@@ -30,7 +71,9 @@ const authUser = asyncHandler(async (req, res) => {
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
 
+  // Check if user exists
   if (user) {
+    // Send back user data
     res.json({
       _id: user._id,
       name: user.name,
@@ -38,9 +81,10 @@ const getUserProfile = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
     })
   } else {
+    // Otherwise send back 404 Error
     res.status(404)
     throw new Error('User Not Found')
   }
 })
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,9 +28,20 @@ const userSchema = mongoose.Schema(
   }
 )
 
+// Compare the hashed and unhashed passwords using bcrypt
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
+
+// Hash the plain-text password anytime it is created/modified
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User', userSchema)
 
