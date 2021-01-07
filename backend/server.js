a58b1cc5d4a244f73411c54e98aa09302485e2de
John <jgilpn@gmail.com>
2021-01-07 14:46:15 -0800

    prepared for production/deployment

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json())
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('API is running')
-})
-
 app.use('/api/v1/products', productRoutes)
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/orders', orderRoutes)
@@ -46,6 +42,21 @@ app.get('/api/v1/config/paypal', (req, res) =>
 // Make uploads folder STATIC
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+// In Production, make frontend/build folder STATIC
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  // Anything not pointing to the /api/v1/* will
+  // point to the frontend index.html
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 // Error Middleware
 app.use(notFound)
